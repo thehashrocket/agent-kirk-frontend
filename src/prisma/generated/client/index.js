c5f2e0029811cd7fb6b16eae079a27ac60d2bd60
Jason Shultz <jasshultz@gmail.com>
2025-05-28 19:19:07 -0700

    added @next-auth/prisma-adapter

--- src/prisma/generated/client/index.js
+++ src/prisma/generated/client/index.js
@@ -532,11 +532,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": true,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:e5b38d4ad2b8a321d538a5239b45@localhost:5432/kirk-frontend"
+        "value": null
       }
     }
   },
