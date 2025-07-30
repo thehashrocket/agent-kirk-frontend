71204c3b25ab5cacc0f10883dde96de23316eb95
Jason Shultz <jasshultz@gmail.com>
2025-07-30 14:47:55 -0700

    fixed typo in log. updated footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -35,22 +35,35 @@ export function Footer() {
   return (
     <footer className="border-t bg-gradient-to-r from-primary/5 via-primary/10 to-background backdrop-blur-lg supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto py-6 px-4">
-        <div className="flex flex-col items-center justify-center space-y-4">
+        {/* Show items in a row and justify-between space between them. */}
+        <div className="flex flex-row justify-between space-x-4">
           <Link
             href="/"
             className="flex items-center space-x-2 font-medium text-gray-900 hover:text-primary/90 transition-colors"
           >
-            <Image 
-              src="/images/Agent-Kirk-Primary-Horizontal-Logo_Color-CMYK.svg" 
-              alt="Agent Kirk" 
-              width={100} 
-              height={100} 
+            <Image
+              src="/images/Agent-Kirk-Primary-Horizontal-Logo_Color-CMYK.svg"
+              alt="Agent Kirk"
+              width={150}
+              height={150}
               className="h-8 w-auto"
             />
           </Link>
           <p className="text-sm text-muted-foreground text-center">
             © {currentYear} Agent Kirk. All rights reserved.
           </p>
+          <Link
+            href="https://1905newmedia.com"
+            className="flex items-center space-x-2 font-medium text-gray-900 hover:text-primary/90 transition-color"
+          >
+            <Image
+              src="/images/1905-New-Media-Primary-Horizontal Logo_Color-RGB.svg"
+              alt="1905 New Media"
+              width={150}
+              height={150}
+              className="h-8 w-auto"
+            />
+          </Link>
         </div>
       </div>
     </footer>
